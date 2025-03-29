110d4c8386e3a39f18008fdcaa3db71531770887
alfr0x800 <199596498+alfr0x800@users.noreply.github.com>
2025-03-29 22:42:56 +0000

    Small formatting change

--- CHIP8/Decode.h
+++ CHIP8/Decode.h
@@ -4,41 +4,10 @@
 
 enum struct Op
 {
-    None,
-    Cls00E0,
-    Ret00EE,
-    Jp1nnn,
-    Call2nnn,
-    Se3xnn,
-    Sne4xnn,
-    Se5xy0,
-    Ld6xnn,
-    Add7xnn,
-    Ld8xy0,
-    Or8xy1,
-    And8xy2,
-    Xor8xy3,
-    Add8xy4,
-    Sub8xy5,
-    Shr8xy6,
-    Subn8xy7,
-    Shl8xyE,
-    Sne9xy0,
-    LdAnnn,
-    JpBnnn,
-    RndCxnn,
-    DrwDxyn,
-    SkpEx9E,
-    SknpExA1,
-    LdFx07,
-    LdFx0A,
-    LdFx15,
-    LdFx18,
-    AddFx1E,
-    LdFx29,
-    LdFx33,
-    LdFx55,
-    LdFx65
+    None, Cls00E0, Ret00EE, Jp1nnn, Call2nnn, Se3xnn, Sne4xnn, Se5xy0, Ld6xnn, Add7xnn, Ld8xy0,
+    Or8xy1, And8xy2, Xor8xy3, Add8xy4, Sub8xy5, Shr8xy6, Subn8xy7, Shl8xyE, Sne9xy0, LdAnnn,
+    JpBnnn, RndCxnn, DrwDxyn, SkpEx9E, SknpExA1, LdFx07, LdFx0A, LdFx15, LdFx18, AddFx1E, LdFx29,
+    LdFx33, LdFx55, LdFx65
 };
 
 struct Instruction
